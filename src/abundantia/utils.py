ee0887bcc3df8158a816049bc0f437127d329ffc
ryo.ishii <ryoishii1101@gmail.com>
2022-03-29 23:41:45 +0900

    logger setup impl

---
+++ src/abundantia/utils.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def setup_logger(name: str, level: str = "DEBUG", logfile_path: str | None = None) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    handlers: list[logging.Handler] = [logging.StreamHandler()]
+    if logfile_path is not None:
+        handlers.append(logging.FileHandler(logfile_path))
+
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    for handler in handlers:
+        handler.setLevel(getattr(logging, level, logging.NOTSET))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
