6f5f20b0a7417d0b45d5d97c51be6d4ec8273fe1
ryo.ishii <ryoishii1101@gmail.com>
2022-04-04 09:00:29 +0900

    GMO Coin Impl

--- src/abundantia/adaptors/gmocoin_client.py
+++ src/abundantia/adaptors/gmocoin_client.py
@@ -16,27 +16,39 @@ class GMOCoinClient:
     btc: str = "BTC"
     symbols: tuple[str, ...] = (btc_jpy, btc)
 
-    async def get_executions_by_http(self, symbol: str, count: int = 500, max_executions: int = 100_000):
+    async def get_executions_by_http(
+        self, symbol: str, page: int = 1, count: int = 100, max_executions: int = 100_000
+    ) -> list[GMOCoinExecution]:
+
         assert symbol in self.symbols
         count = min(count, max_executions)
 
         all_executions: list[GMOCoinExecution] = []
-        params = {"symbol": symbol}
+        params = {"symbol": symbol, "page": page, "count": count}
 
         async with pybotters.Client(base_url=self.http_url) as client:
-
             while len(all_executions) < max_executions:
                 try:
                     response = await client.get("/v1/trades", params=params)
-                    executions = await response.json()
-                    executions = executions.get("data", {}).get("list", [])
+                    response = await response.json()
+                    data = response.get("data", {})
+                    executions = data.get("list", [])
                     all_executions += [GMOCoinExecution(**e) for e in executions]
+                    current_page = data.get("pagination", {}).get("currentPage", None)
 
-                    await asyncio.sleep(1)
+                    if current_page is None:
+                        logger.warn(f"pagination error. {data}")
+                        break
 
                     if len(executions) != count:
                         logger.warn(f"{len(executions)} != {count}.")
                         break
+
+                    params["page"] = current_page + 1
+
+                    logger.info(params)
+                    logger.info(f"{all_executions[0].timestamp}, {all_executions[-1].timestamp}, {len(all_executions)}")
+                    await asyncio.sleep(1)
 
                 except Exception:
                     logger.error(traceback.format_exc())
