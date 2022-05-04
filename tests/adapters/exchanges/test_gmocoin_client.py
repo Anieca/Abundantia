57cf5e903e9f7953a0f71049cd7f4efce8c41474
ryo.ishii <ryoishii1101@gmail.com>
2022-05-04 14:39:44 +0900

    mod test settings

--- tests/adapters/exchanges/test_gmocoin_client.py
+++ tests/adapters/exchanges/test_gmocoin_client.py
@@ -5,37 +5,36 @@ import pytest
 from abundantia.adapters import GMOCoinClient
 from abundantia.schema.gmocoin import GMOCoinExecution
 
-client = GMOCoinClient(log_level="WARNING")
 
+class TestGMOCoinClient:
+    def setup_method(self):
+        self.client = GMOCoinClient(log_level="WARNING")
 
-@pytest.mark.integration
-def test_get_klines_by_http():
-    klines = client.get_klines_by_http(GMOCoinClient.symbols.BTC_JPY, 60, datetime(2021, 4, 15))
-    assert len(klines) == 60 * 24
+    @pytest.mark.exchange
+    def test_get_klines_by_http(self):
+        klines = self.client.get_klines_by_http(GMOCoinClient.symbols.BTC_JPY, 60, datetime(2021, 4, 15))
+        assert len(klines) == 60 * 24
 
+    @pytest.mark.exchange
+    def test_get_executions_by_http(self):
+        count = 50
+        executions = self.client.get_executions_by_http(GMOCoinClient.symbols.BTC_JPY, max_executions=count)
+        assert len(executions) == count
 
-@pytest.mark.integration
-def test_get_executions_by_http():
-    count = 50
-    executions = client.get_executions_by_http(GMOCoinClient.symbols.BTC_JPY, max_executions=count)
-    assert len(executions) == count
+    def test_convert_executions_to_common_klines(self):
+        executions = [
+            GMOCoinExecution(price=5010271.0, side="SELL", size=0.01, timestamp="2022-04-12T14:48:01.828Z"),
+            GMOCoinExecution(price=5010271.0, side="BUY", size=0.01, timestamp="2022-04-12T14:48:01.828Z"),
+        ]
+        symbol = self.client.symbols.BTC_JPY
+        interval = 60
 
+        klines = self.client.convert_executions_to_common_klines(symbol, interval, executions, inclusive="neither")
+        assert len(klines) == 0
 
-def test_convert_executions_to_common_klines():
-    executions = [
-        GMOCoinExecution(price=5010271.0, side="SELL", size=0.01, timestamp="2022-04-12T14:48:01.828Z"),
-        GMOCoinExecution(price=5010271.0, side="BUY", size=0.01, timestamp="2022-04-12T14:48:01.828Z"),
-    ]
-    symbol = GMOCoinClient.symbols.BTC_JPY
-    interval = 60
+        klines = self.client.convert_executions_to_common_klines(symbol, interval, executions, inclusive="both")
+        assert len(klines) == 1
 
-    klines = client.convert_executions_to_common_klines(symbol, interval, executions, inclusive="neither")
-    assert len(klines) == 0
-
-    klines = client.convert_executions_to_common_klines(symbol, interval, executions, inclusive="both")
-    assert len(klines) == 1
-
-
-def test_convert_datetime_to_specific():
-    assert "20220101" == GMOCoinClient.convert_datetime_to_specific(datetime(2022, 1, 1))
-    assert "20220101" == GMOCoinClient.convert_datetime_to_specific(datetime(2022, 1, 1, 1, 1, 1))
+    def test_convert_datetime_to_specific(self):
+        assert "20220101" == self.client.convert_datetime_to_specific(datetime(2022, 1, 1))
+        assert "20220101" == self.client.convert_datetime_to_specific(datetime(2022, 1, 1, 1, 1, 1))
--- tests/pipelines/ingestion/test_ingest_klines.py
+++ tests/pipelines/ingestion/test_ingest_klines.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import pytest
 
+from abundantia.adapters.databases.sqlite_client import SQLiteClient
 from abundantia.pipelines.ingestion.ingest_klines import ingest_klines_to_sqlite_from_gmocoin_klines
 from abundantia.schema import GMOCoinSymbols
 
@@ -12,4 +13,8 @@ def test_ingest_klines_to_sqlite_from_gmocoin_klines():
     interval = 60
     start_date = datetime(2021, 4, 15)
     end_date = datetime(2021, 4, 20)
+
+    sqlite = SQLiteClient()
+    sqlite.create_tables()
     ingest_klines_to_sqlite_from_gmocoin_klines(symbol, interval, start_date, end_date)
+    sqlite.drop_common_kline_table()
