23a9ff7bf6de62d1d874cfa1a7c72f5ae1a81d86
ryo.ishii <ryoishii1101@gmail.com>
2022-05-04 13:41:06 +0900

    impl ingest_klines

--- src/abundantia/pipelines/ingestion/__init__.py
+++ src/abundantia/pipelines/ingestion/__init__.py
@@ -1,2 +1,2 @@
 # flake8: noqa
-from abundantia.pipelines.ingestion.ingest_klines_from_gmocoin import ingest_gmocoin_klines_to_sqlite
+from abundantia.pipelines.ingestion.ingest_klines import ingest_klines_to_sqlite_from_gmocoin_klines
