ea7af25a3ad19b8a4c20e2339abf4f42c573cc0d
ryo.ishii <ryoishii1101@gmail.com>
2022-05-06 15:46:08 +0900

    add types-python-dateutil for gha and pre-commit

--- tests/pipelines/ingestion/test_ingest_klines.py
+++ tests/pipelines/ingestion/test_ingest_klines.py
@@ -2,8 +2,11 @@ from datetime import datetime
 
 import pytest
 
-from abundantia.pipelines.ingestion.ingest_klines import ingest_klines_to_sqlite_from_gmocoin_klines
-from abundantia.schema import GMOCoinSymbols
+from abundantia.pipelines.ingestion.ingest_klines import (
+    ingest_klines_to_sqlite_from_bitflyer_klines,
+    ingest_klines_to_sqlite_from_gmocoin_klines,
+)
+from abundantia.schema import BitFlyerSymbols, GMOCoinSymbols
 
 
 @pytest.mark.integration
@@ -13,3 +16,12 @@ def test_ingest_klines_to_sqlite_from_gmocoin_klines():
     start_date = datetime(2021, 4, 15)
     end_date = datetime(2021, 4, 20)
     ingest_klines_to_sqlite_from_gmocoin_klines(symbol, interval, start_date, end_date)
+
+
+@pytest.mark.integration
+def test_ingest_klines_to_sqlite_from_bitflyer_executions():
+    symbol = BitFlyerSymbols.FX_BTC_JPY
+    interval = 60
+    start_date = datetime(2022, 5, 1)
+    end_date = datetime(2022, 5, 3)
+    ingest_klines_to_sqlite_from_bitflyer_klines(symbol, interval, start_date, end_date)
