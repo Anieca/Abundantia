59917cf33008c15ffdd2d25e5f4af21ea8d06b78
ryo.ishii <ryoishii1101@gmail.com>
2022-05-06 17:12:38 +0900

    test 削除

--- tests/pipelines/ingestion/test_ingest_klines.py
+++ tests/pipelines/ingestion/test_ingest_klines.py
@@ -3,11 +3,8 @@ from datetime import datetime
 import pytest
 
 from abundantia.adapters import SQLiteClient
-from abundantia.pipelines.ingestion.ingest_klines import (
-    ingest_klines_to_sqlite_from_bitflyer_klines,
-    ingest_klines_to_sqlite_from_gmocoin_klines,
-)
-from abundantia.schema import BitFlyerSymbols, GMOCoinSymbols
+from abundantia.pipelines.ingestion.ingest_klines import ingest_klines_to_sqlite_from_gmocoin_klines
+from abundantia.schema import GMOCoinSymbols
 
 
 @pytest.mark.integration
@@ -20,12 +17,3 @@ def test_ingest_klines_to_sqlite_from_gmocoin_klines():
     sqlite = SQLiteClient()
     sqlite.create_tables()
     ingest_klines_to_sqlite_from_gmocoin_klines(symbol, interval, start_date, end_date)
-
-
-@pytest.mark.integration
-def test_ingest_klines_to_sqlite_from_bitflyer_executions():
-    symbol = BitFlyerSymbols.FX_BTC_JPY
-    interval = 60
-    start_date = datetime(2022, 5, 1)
-    end_date = datetime(2022, 5, 3)
-    ingest_klines_to_sqlite_from_bitflyer_klines(symbol, interval, start_date, end_date)
