85a3061de678bb8e2d4530f9b2c144aebf7f240a
ryo.ishii <ryoishii1101@gmail.com>
2022-05-06 15:20:04 +0900

    sqlalchemy model は切り出し

---
+++ src/abundantia/schema/model.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Float, Integer, String, Table
+
+from abundantia.adapters.databases.mapper import mapper_registry
+from abundantia.schema import CommonKline
+
+
+@mapper_registry.mapped
+class CommonKlineModel(CommonKline):
+
+    __table__ = Table(
+        "common_kline",
+        mapper_registry.metadata,
+        Column("exchange", String(128), primary_key=True),
+        Column("symbol", String(128), primary_key=True),
+        Column("interval", Integer, primary_key=True),
+        Column("open_time", Integer, primary_key=True),
+        Column("open", Float),
+        Column("high", Float),
+        Column("low", Float),
+        Column("close", Float),
+        Column("volume", Float),
+    )
