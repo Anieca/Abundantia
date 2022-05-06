996a3d0f9ec47f1ad37ddaab8d62f5815ef6dfd9
ryo.ishii <ryoishii1101@gmail.com>
2022-05-06 15:20:54 +0900

    log name の修正, import 修正

--- src/abundantia/adapters/databases/sqlite_client.py
+++ src/abundantia/adapters/databases/sqlite_client.py
@@ -9,14 +9,15 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from abundantia.adapters.databases.mapper import mapper_registry
-from abundantia.schema.common import CommonKlineModel, CommonKlineSchema
+from abundantia.schema import CommonKlineSchema
+from abundantia.schema.model import CommonKlineModel
 from abundantia.utils import setup_logger
 
 
 class SQLiteClient:
     def __init__(self, file_path: str = "sqlite:///resources/db.sqlite3", log_level: str = "DEBUG") -> None:
         self.engine = sqlalchemy.create_engine(file_path, echo=False)
-        self.logger = setup_logger(__name__, log_level)
+        self.logger = setup_logger(self.__class__.__name__, log_level)
 
     def create_tables(self) -> None:
         mapper_registry.metadata.create_all(bind=self.engine)
